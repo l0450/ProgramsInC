600823f9654b8a55ebd713a9424ba57cb4e6b26f
l0450 <61821254+l0450@users.noreply.github.com>
2025-10-24 16:15:30 +0200

    Update Exercise2.c file

--- Exercise2.c
+++ Exercise2.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double dochod;
+    double podatek;
+
+    printf("Podaj swoj dochod roczny:\n");
+    scanf("%lf", &dochod);
+
+    if (dochod <= 50000){
+        podatek = 10.00;
+        printf("Wysokosc podatku wynosi: %.2lf\n", podatek);
+    } else if (dochod > 50000 && dochod <= 100000) {
+        podatek = 15.00;
+        printf("Wysokosc podatku wynosi: %.2lf\n", podatek);
+    } else {
+        podatek = 20.00;
+        printf("Wysokosc podatku wynosi: %.2lf\n", podatek);
+    }
+
+
+
+
+    return 0;
+}
 
