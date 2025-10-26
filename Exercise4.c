76841d79b4a8deca17210ad6f946a55da2ffde3f
l0450 <61821254+l0450@users.noreply.github.com>
2025-10-26 15:59:31 +0100

    Update Exercise4.c file
    A program that implements the Fibonacci sequence calculation and returns a number that is presented at a specific point where Fibonacci sequence is done.

--- Exercise4.c
+++ Exercise4.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
+
+int main()
+{
+    int number;
+    printf("Wskaz liczbe ktora wystepuje w ciagu Fibonacciego: ");
+    scanf("%d", &number);
+
+    int result = fibonacci(number);
+
+    printf("Dana liczba %d w ciagu Fibonacciego to: %d", number, result);
+
+    return 0;
+}
 
