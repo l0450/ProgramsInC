560ac12787f35368e37d64d04c436e43aa2ece04
l0450 <61821254+l0450@users.noreply.github.com>
2025-10-26 15:53:17 +0100

    Update Exercise3.c file
    A recursive function to return the sum of numbers shown in an initialized array

--- Exercise3.c
+++ Exercise3.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sumaTablicy(int tablica[], int index, int n) {
+    if (index >= n) {
+        return 0;
+    } else {
+        return tablica[index] + sumaTablicy(tablica, index + 1, n);
+    }
+}
+
+int main()
+{
+    int tablica[] = {8, 9, 1, 2, 5};
+    int rozmiarTablicy = 5;
+    int suma = sumaTablicy(tablica, 0, rozmiarTablicy);
+
+    printf("Suma elementow w danej tablicy wynosi: %d", suma);
+
+    return 0;
+}
 
